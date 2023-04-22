61adecac3da6bbaa1eac7b9fb42c460508b217a3
Jeison Rosario <jeisonrosario5@gmail.com>
2023-04-22 19:04:36 -0400

    Move file 'templetes/templatepdf.py' -> '/templetepdf.py'

---
+++ templatepdf.py
@@ -0,0 +1,6 @@
+templatePdf = """Archivo adjunto(PDF) enviando enviado mediante el protocolo smtp<br />
+El fin ultimo del archivo es ser Personalizado dependiendo del receptor del archivo<br /><br />
+Receptor[Email]: {}<br /><br />
+Receptor[Nombre]: {}<br /><br />
+Fecha creacion: {}<br /><br />
+"""
