63d6c8728054685b79548f263a57ee2787c0e696
Jeison Rosario <jeisonrosario5@gmail.com>
2023-04-22 19:13:45 -0400

    Update

--- send_mail.py
+++ send_mail.py
@@ -10,22 +10,23 @@ sendMail: Objeto creado para la resolución de ciclo. Se compone de :
     Método, encargado del mecanismo de envió la plantilla html por correo
 """
 
-import pandas as pd
-import smtplib, ssl
-from email.message import EmailMessage
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
 import os
 import time
-from templetes.templetehtml import cuerpo_html # Templete
+import smtplib, ssl
+import pandas as pd
 from email import encoders
+from datetime import datetime
 from email.mime.base import MIMEBase
-from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from reportlab.pdfgen.canvas import Canvas
+from email.message import EmailMessage
+from email.mime.multipart import MIMEMultipart
+from create_pdfFile import PdfTools
+from templetehtml import cuerpo_html # Templete
+from dateutil.relativedelta import relativedelta
 df = pd.read_csv('contactos.csv')
 
-class sendMail:
+#instancia de clase PdfTools
+class SendMail:
     """_summary_
     Envia correo electronicos perzonalidos
     """
@@ -35,14 +36,14 @@ class sendMail:
         Args:
             email (mail): Direccion email del remitente
             password (pass): Contraseña para aplicaciones
-            cuerpo_html (template): Plantilla de correo html
         """
-        global cuerpo_html
         self.email = email
         self.password = password
         self.cuerpo_html = cuerpo_html
         self.context = ssl.create_default_context()
-        
+        # Hora-Fecha - Informacion adicional
+        named_tuple = time.localtime() # get struct_time
+        self.time_string = time.strftime("%m/%d/%Y, %H:%M:%S", named_tuple)
 
     def send_mail_html(self, df):
         """_summary_
@@ -50,11 +51,8 @@ class sendMail:
         Args:
             df (dataframe): Archivo csv con la informacion
         """
-        remitente = self.email
-
-        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=self.context) as servidor:
-            servidor.login(self.email, self.password)
-            
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=self.context) as server:
+            server.login(self.email, self.password)
             # Filtramos los datos para que sólamente se envíen los correos a las personas que cumplen años en el día de hoy
             # hoy = datetime.now().strftime('%d/%m') # La fecha del dia (Dia/Mes)
             datos_hoy = df[df['date'].apply(lambda x: datetime.strptime(f"{datetime.now().year}/{x[-2:]}/{x[:5]}", '%Y/%y/%d/%m').strftime('%d/%m') == datetime.now().strftime('%d/%m'))]
@@ -62,23 +60,22 @@ class sendMail:
             # Recorremos el DataFrame con un ciclo for y enviamos un correo electrónico a cada destinatario
             for column, row in datos_hoy.iterrows():
                 nombreDestinatario = row['nombre'].upper()
+                
                 # Calcular edad ['date']
                 fecha_nacimiento = datetime.strptime(row['date'], "%d/%m/%Y")
                 edad = relativedelta(datetime.now(), fecha_nacimiento)
+                
                 # plantilla html con los datos formateados | str.format(nombreDestinatario, edad.years)
                 cuerpo_html = self.cuerpo_html.format(nombreDestinatario, edad.years)
-                destinatario = row['mail']
                 mensaje = EmailMessage()
-                mensaje['From'] = remitente
-                mensaje['To'] = destinatario
+                mensaje['From'] = self.email
+                mensaje['To'] = row['mail']
                 mensaje['Subject'] = 'Feliz Cumpleaños!!'
                 mensaje.set_content(cuerpo_html, subtype='html')
-                servidor.send_message(mensaje)
+                server.send_message(mensaje)
     
     def send_mail_text(self, df):
-        # Hora-Fecha - Informacion adicional
-        named_tuple = time.localtime() # get struct_time
-        time_string = time.strftime("%m/%d/%Y, %H:%M:%S", named_tuple)
+        
         mensaje = """\
         Subject: Prueba smtplib
 
@@ -89,16 +86,15 @@ class sendMail:
             for column, row in df.iterrows():
                 destinatario = row['mail']
                 nombreDestinatario = row['nombre']
-                mensaje = mensaje.format(nombreDestinatario,time_string)
+                mensaje = mensaje.format(nombreDestinatario,self.time_string)
                 server.sendmail(self.email, destinatario, mensaje)
 
     def send_mail_pdf(self, df):
-        pathPdf = "ejemplo_2.pdf"
-        pdfFile = Canvas(pathPdf)  # In same directory as script
+        archivoPdf_pach = "tempfile.pdf"
+        pdfTools = PdfTools(archivoPdf_pach) # Instancia de objeto "PdfTools"
         cuerpoTexto = "Este es el contenido en texto del mensaje"
         sender_email = self.email
-        receiver_email = os.environ['RETO3USER']
-        password = os.environ['RETO3PASS']
+        password = self.password
         # Log in to server using secure context and send email
         with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=self.context) as server:
             server.login(sender_email, password)
@@ -107,38 +103,41 @@ class sendMail:
                 # Create a multipart message and set headers
                 message = MIMEMultipart()
                 message["From"] = sender_email
-                message["To"] = receiver_email
-                message["Subject"] = "An email with attachment from Python"
-                message["Bcc"] = receiver_email  # Recommended for mass emails
+                message["To"] = row['mail']
+                message["Subject"] = "Email enviado desde python"
+                message["Bcc"] = row['mail']  # Recommended for mass emails
                 # Add cuerpoTexto to email
                 message.attach(MIMEText(cuerpoTexto, "plain"))
-                pdfFile.drawString(72, 72, f"Hello, World{row['nombre']}")
-
+                
+                # Metodo para crear el pdf
+                pdfTools.create_pdf(row['mail'],row['nombre'],self.time_string) 
                 # Open PDF file in binary mode
-                with open(pathPdf, "rb") as attachment:
+                with open(archivoPdf_pach, "rb") as attachment:
                     # Add file as application/octet-stream
                     # Email client can usually download this automatically as attachment
                     part = MIMEBase("application", "octet-stream")
                     part.set_payload(attachment.read())
-
-
                 # Encode file in ASCII characters to send by email    
                 encoders.encode_base64(part)
-
                 # Add header as key/value pair to attachment part
                 part.add_header(
                     "Content-Disposition",
-                    f"attachment; filename= {pdfFile}",
+                    f"attachment; filename= {archivoPdf_pach}",
                 )
-
                 # Add attachment to message and convert message to string
                 message.attach(part)
                 text = message.as_string()
-
-            
-                server.sendmail(sender_email, receiver_email, text)
-
+                server.sendmail(sender_email, row['mail'], text)
+                pdfTools.delete_pdf()
 
 # Instanciamos la clase sendMain
-send_mail = sendMail(os.environ['RETO3USER'], os.environ['RETO3PASS'])
-send_mail.send_mail_pdf(df)
+send_mail = SendMail(os.environ['RETO3USER'], os.environ['RETO3PASS'])
+
+# Metodos de envio
+""" 
+Toda la informacion necesaria para el envio del correo se entra dentro de la clase SendMail
+"""
+# send_mail.send_mail_text(df)
+# send_mail.send_mail_html(df)
+# send_mail.send_mail_pdf(df)
+
---
+++ templetehtml.py
@@ -0,0 +1,67 @@
+"""
+**********************************************************
+Opte por guardar la plantilla en una variable de python 
+para poder usar el formato de cadenas (str.format)
+**********************************************************
+Plantilla de correo, recibe 2 datos con (str.format)
+nombre y edad del que recibe el correo para un resultado mas personalizado
+https://stripo.email/: Generador de plantillas html para emails
+"""
+
+cuerpo_html = """
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
+<body>
+    <div class="es-wrapper-color">
+        <table class="es-wrapper" width="100%" cellspacing="0" cellpadding="0" style="background-position: center top;">
+            <tbody>
+                <tr>
+                    <td class="esd-email-paddings" valign="top">
+                        <table cellpadding="0" cellspacing="0" class="es-content esd-footer-popover" align="center">
+                            <tbody>
+                                <tr>
+                                    <td class="esd-stripe" align="center">
+                                        <table bgcolor="#ffffff" class="es-content-body" align="center" cellpadding="0" cellspacing="0" width="600">
+                                            <tbody>
+                                                <tr>
+                                                    <td class="es-p20t es-p20r es-p20l esd-structure" align="left" background="https://demo.stripocdn.email/content/guids/3bd8ea22-deb1-43ca-a8a4-52333ae7eafd/images/template.png" style="background-image: url(https://demo.stripocdn.email/content/guids/3bd8ea22-deb1-43ca-a8a4-52333ae7eafd/images/template.png); background-repeat: no-repeat; background-position: left top;">
+                                                        <table cellpadding="0" cellspacing="0" width="100%">
+                                                            <tbody>
+                                                                <tr>
+                                                                    <td width="560" class="esd-container-frame" align="center" valign="top">
+                                                                        <table cellpadding="0" cellspacing="0" width="100%">
+                                                                            <tbody>
+                                                                                <tr>
+                                                                                    <td align="left" class="esd-block-text">
+                                                                                        <p style="text-align: center; font-family: 'comic sans ms', 'marker felt-thin', arial, sans-serif; font-size: 19px; color: #cc66cc;">CON MUCHO CARIÑO PARA {}<br>TE QUIERO:</p>
+                                                                                        <p style="text-align: center; font-family: 'comic sans ms', 'marker felt-thin', arial, sans-serif; font-size: 17px;"><br></p>
+                                                                                        <h1 style="text-align: center; color: #9933cc;">!TE DESEO<br>FELIZ<br>CUMPLEAÑOS¡</h1>
+                                                                                        <p style="font-size: 17px;"><br style="font-size: 17px;"></p>
+                                                                                        <p style="text-align: center; font-family: 'comic sans ms', 'marker felt-thin', arial, sans-serif; color: #cc66cc; font-size: 18px;">Dicen que las palabras se las lleva el tiempo..</p>
+                                                                                        <p style="text-align: center; font-family: 'comic sans ms', 'marker felt-thin', arial, sans-serif; color: #cc66cc; font-size: 18px;">Pero esta imagen vale mas que mil palabras.</p>
+                                                                                        <p style="text-align: center; font-family: 'comic sans ms', 'marker felt-thin', arial, sans-serif; color: #cc66cc; font-size: 18px;">Felices {} Años</p>
+                                                                                        <p><br></p>
+                                                                                        <p style="display: none;"><br></p>
+                                                                                    </td>
+                                                                                </tr>
+                                                                            </tbody>
+                                                                        </table>
+                                                                    </td>
+                                                                </tr>
+                                                            </tbody>
+                                                        </table>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
+"""
